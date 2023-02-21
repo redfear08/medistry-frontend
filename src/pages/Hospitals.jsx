ef957d4f56b0aad1d08f6b126d93ef9f9293842d
redfear08 <sandipan.bera94@gmail.com>
2023-02-21 20:58:10 +0530

    intial commit

---
+++ src/pages/Hospitals.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hospitals = () => {
+    return (
+        <div>
+            <h1>Hospitals page</h1>
+        </div>
+    );
+};
+
+export default Hospitals;
---
+++ src/pages/Medicalhistory.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Medicalhistory  = () => {
+    return (
+        <div>
+            <h1>Medical History page</h1>
+        </div>
+    );
+};
+
+export default Medicalhistory;
